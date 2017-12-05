02c61ff52c087ab6192d3d9f8a6842c8d759db6f
seabasschang <34096922+seabasschang@users.noreply.github.com>
2017-12-05 08:14:27 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+import sys
+
+# What does end='' do in the print statements?
+# Respond here: Forces the text in each statement to end on one line. 
+print('Hello, ', end='')
+print(sys.argv[1], end='')
+print('!')
