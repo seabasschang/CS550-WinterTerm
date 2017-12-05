956f4d1e1052c9dd6a9ac3ac24876bd4ab29acb8
seabasschang <34096922+seabasschang@users.noreply.github.com>
2017-12-05 08:16:38 -0500

    Interactive Version

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 import sys
 
-# What does end='' do in the print statements?
-# Respond here: Forces the text in each statement to end on one line. 
 name = str(input('What is your name? '))
 print('Hello, '+name+'!')
